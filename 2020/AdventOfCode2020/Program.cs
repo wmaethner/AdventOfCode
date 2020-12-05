6a0ba5ceb553259ce1bef4335068b9ddd2a37894
Will Maethner <32457351+wmaethner@users.noreply.github.com>
2020-12-04 21:05:09 -0600

    Day 4

--- 2020/AdventOfCode2020/Program.cs
+++ 2020/AdventOfCode2020/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
+using System.Text.RegularExpressions;
+using Validators;
 
 namespace AdventOfCode2020
 {
@@ -160,6 +163,223 @@ namespace AdventOfCode2020
         }
         #endregion
 
+        #region Day 3
+        private static void Day3()
+        {
+            string[] fileContents = HelperFunctions.FileContents(3, 1);
+            //string[] fileContents = {"..##.......",
+            //                        "#...#...#..",
+            //                        ".#....#..#.",
+            //                        "..#.#...#.#",
+            //                        ".#...##..#.",
+            //                        "..#.##.....",
+            //                        ".#.#.#....#",
+            //                        ".#........#",
+            //                        "#.##...#...",
+            //                        "#...##....#",
+            //                        ".#..#...#.#", };
+
+
+            //Variables
+            int width = fileContents[0].Length;
+            Int64 treeProduct = 1;
+            List<Tuple<int, int>> slopes = new List<Tuple<int, int>>();
+            slopes.Add(Tuple.Create<int, int>(1, 1));
+            slopes.Add(Tuple.Create<int, int>(3, 1));
+            slopes.Add(Tuple.Create<int, int>(5, 1));
+            slopes.Add(Tuple.Create<int, int>(7, 1));
+            slopes.Add(Tuple.Create<int, int>(1, 2));
+
+            foreach (Tuple<int, int> slope in slopes)
+            {
+                int curX = 0, curY = 0;
+                int dX = slope.Item1, dY = slope.Item2;
+                int treeCount = 0;
+
+                while (curY < fileContents.Length)
+                {
+                    if (fileContents[curY][curX] == '#')
+                    {
+                        treeCount++;
+                    }
+
+                    curX = (curX + dX) % width;
+                    curY += dY;
+                }
+
+                treeProduct *= treeCount;
+            }
+
+            Console.WriteLine($"Tree product: {treeProduct}");
+        }
+        #endregion
+
+        #region Day 4
+        private static void Day4()
+        {
+            string[] fileContents = HelperFunctions.FileContents(4, 1);
+            List<Passport> passports = new List<Passport>();
+
+            List<string> passportLines = new List<string>();
+            foreach (string line in fileContents)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    passports.Add(new Passport(passportLines));
+                    passportLines.Clear();
+                    continue;
+                }
+
+                passportLines.Add(line);
+            }
+            if (passportLines.Count > 0)
+            {
+                passports.Add(new Passport(passportLines));
+            }
+
+            int count = 0;
+            foreach (Passport passport in passports)
+            {
+                if (passport.Valid())
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Valid passports: {count}");
+        }
+
+        class Passport
+        {
+            #region Properties
+            public string byr { get; set; }
+            public string iyr { get; set; }
+            public string eyr { get; set; }
+            public string hgt { get; set; }
+            public string hcl { get; set; }
+            public string ecl { get; set; }
+            public string pid { get; set; }
+            public string cid { get; set; }
+            #endregion
+
+            public Passport(List<string> lines)
+            {
+                foreach (string line in lines)
+                {
+                    string[] props = line.Split(" ");
+                    foreach (string prop in props)
+                    {
+                        string key = prop.Split(":")[0];
+                        string value = prop.Split(":")[1];
+
+                        switch (key)
+                        {
+                            case "byr":
+                                byr = value;
+                                break;
+                            case "iyr":
+                                iyr = value;
+                                break;
+                            case "eyr":
+                                eyr = value;
+                                break;
+                            case "hgt":
+                                hgt = value;
+                                break;
+                            case "hcl":
+                                hcl = value;
+                                break;
+                            case "ecl":
+                                ecl = value;
+                                break;
+                            case "pid":
+                                pid = value;
+                                break;
+                            case "cid":
+                                cid = value;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            public bool Valid()
+            {
+                ////For now missing cid is ok
+                if (string.IsNullOrEmpty(byr) ||
+                    string.IsNullOrEmpty(iyr) ||
+                    string.IsNullOrEmpty(eyr) ||
+                    string.IsNullOrEmpty(hgt) ||
+                    string.IsNullOrEmpty(hcl) ||
+                    string.IsNullOrEmpty(ecl) ||
+                    string.IsNullOrEmpty(pid))
+                {
+                    return false;
+                }
+
+                if (!ValidYear(byr, 1920, 2002) ||
+                    !ValidYear(iyr, 2010, 2020) ||
+                    !ValidYear(eyr, 2020, 2030) ||
+                    !ValidHeight(hgt) ||
+                    !ValidHairColor(hcl) ||
+                    !ValidEyeColor(ecl) ||
+                    !ValidPassportId(pid))
+                {
+                    return false;
+                }
+
+              
+                return true;
+            }
+
+            private bool ValidYear(string value, int min, int max)
+            {
+                return value.ValidNumberRange(min, max);
+            }
+
+            private bool ValidHeight(string value)
+            {
+                if (value.Contains("cm"))
+                {
+                    return value.Replace("cm", "").ValidNumberRange(150, 193);
+                }
+
+                if (value.Contains("in"))
+                {
+                    return value.Replace("in", "").ValidNumberRange(59, 76);
+                }
+
+                return false;
+            }
+
+            private bool ValidHairColor(string value)
+            {
+                return Regex.IsMatch(value, @"#[0-9a-f]{6}");
+            }
+
+            private bool ValidEyeColor(string value)
+            {
+                switch (value)
+                {
+                    case "amb":
+                    case "blu":
+                    case "brn":
+                    case "gry":
+                    case "grn":
+                    case "hzl":
+                    case "oth":
+                        return true;
+                }
+
+                return false;
+            }
+
+            private bool ValidPassportId(string value)
+            {
+                return Regex.IsMatch(value, @"^[0-9]{9}$");
+            }
+        }
+        #endregion
         #endregion
     }
 }
---
+++ 2020/Validators1/NumberValidator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Validators
+{
+    public static class NumberValidator
+    {
+
+        public static bool IsNumeric(this string str) => double.TryParse(str, out _);
+        public static bool IsInteger(this string str) => int.TryParse(str, out _);
+
+        #region Converters
+        public static double ToDouble(this string str)
+        {
+            double dbl;
+
+            if (!double.TryParse(str, out dbl))
+            {
+                throw new ArgumentException($"String {str} is not castable to a double.");
+            }
+
+            return dbl;
+        }
+        public static int? ToInt(this string str)
+        {
+            int num;
+
+            if (!int.TryParse(str, out num))
+            {
+                throw new ArgumentException($"String {str} is not castable to a int.");
+            }
+
+            return num;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="min">inclusive</param>
+        /// <param name="max">inclusive</param>
+        /// <returns></returns>
+        public static bool ValidNumberRange(this string str, int min, int max)
+        {
+            if (!str.IsNumeric())
+            {
+                return false;
+            }
+
+            double number = str.ToDouble();
+            return ValidNumberRange(number, min, max);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="min">inclusive</param>
+        /// <param name="max">inclusive</param>
+        /// <returns></returns>
+        public static bool ValidNumberRange(this int number, int min, int max)
+        {
+            return number >= min && number <= max;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="min">inclusive</param>
+        /// <param name="max">inclusive</param>
+        /// <returns></returns>
+        public static bool ValidNumberRange(this double number, int min, int max)
+        {
+            return number >= min && number <= max;
+        }
+    }
+}
